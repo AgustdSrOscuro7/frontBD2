b47f121b09ecc6b69e0a345a150f5b20b32282a3
AgustdSrOscuro7 <valekrudas1@gmail.com>
2024-06-05 08:44:21 -0500

    commit

--- src/app/services/mongo-db.service.ts
+++ src/app/services/mongo-db.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Heroe } from '../interfaces/heroe.interface';
 import { URL_SERVICIOS_MONGODB } from '../config/url.servicios';
-import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,41 +12,53 @@ export class MongoDBService {
 
   constructor(private http: HttpClient) { }
 
-  getHeroes(): Observable<any> {
+  getHeroes() {
     const url = `${URL_SERVICIOS_MONGODB}/api/heroes`;
-    return this.http.get(url).pipe(map(data => {
-      console.log('DATOS', data);
-      return data;
-    }));
+    return this.http.get(url).pipe(
+      catchError(error => {
+        console.error('Error al obtener los héroes:', error);
+        return throwError(error);
+      })
+    );
   }
 
-  getUnHeroe(unId: string): Observable<any> {
+  getUnHeroe(unId: string) {
     const url = `${URL_SERVICIOS_MONGODB}/api/heroes/${unId}`;
-    return this.http.get(url).pipe(map(data => {
-      console.log('DATOS', data);
-      return data;
-    }));
+    return this.http.get<Heroe>(url).pipe( // Especificar el tipo de respuesta como Heroe
+      catchError(error => {
+        console.error('Error al obtener el héroe:', error);
+        return throwError(error);
+      })
+    );
   }
 
-  crud_Heroes(unHeroe: Heroe, unaAccion: string): Observable<any> {
-    const url = `${URL_SERVICIOS_MONGODB}/api/heroes`;
-
-    if (unaAccion === 'insertar') {
-      return this.http.post(url, unHeroe).pipe(map(data => data));
-    } else if (unaAccion === 'modificar') {
-      const updateUrl = `${url}/${unHeroe._id}`;
-      return this.http.put(updateUrl, unHeroe).pipe(map(data => data));
-    } else if (unaAccion === 'eliminar') {
-      const deleteUrl = `${url}/${unHeroe._id}`;
-      return this.http.delete(deleteUrl).pipe(map(data => data));
-    } else {
-      // Handle the case where `unaAccion` is not recognized
-      throw new Error(`Unknown action: ${unaAccion}`);
-    }
-  }
-
-  borrarHeroe(id: string): Observable<any> {
+  putHeroe(id: string, heroe: Heroe) {
     const url = `${URL_SERVICIOS_MONGODB}/api/heroes/${id}`;
-    return this.http.delete(url).pipe(map(data => data));
+    return this.http.put(url, heroe).pipe(
+      catchError(error => {
+        console.error('Error al actualizar el héroe:', error);
+        return throwError(error);
+      })
+    );
+  }
+
+  postHeroe(heroe: Heroe) {
+    const url = `${URL_SERVICIOS_MONGODB}/api/heroes`;
+    return this.http.post(url, heroe).pipe(
+      catchError(error => {
+        console.error('Error al crear el héroe:', error);
+        return throwError(error);
+      })
+    );
+  }
+
+  deleteHeroe(id: string) {
+    const url = `${URL_SERVICIOS_MONGODB}/api/heroes/${id}`;
+    return this.http.delete(url).pipe(
+      catchError(error => {
+        console.error('Error al eliminar el héroe:', error);
+        return throwError(error);
+      })
+    );
   }
 }
